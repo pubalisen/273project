dd9ae8f9ce05abfeb921b888436dc38ae0df19ec
pubalisen <pubali.sen@sjsu.edu>
2014-05-01 03:33:14 -0700

    UI enhance-ShareFile

---
+++ src/main/java/edu/sjsu/cmpe/dropbox/ui/views/DeletedFileView.java
@@ -0,0 +1,36 @@
+package edu.sjsu.cmpe.dropbox.ui.views;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.yammer.dropwizard.views.View;
+
+public class DeletedFileView extends View{
+
+	//private List<String> filenames;
+		List<FilesForMustach> listOfFiles =new ArrayList<FilesForMustach>();
+		
+		public DeletedFileView(List<FilesForMustach> listOfFiles) {
+			super("deletedFiles.mustache");
+			this.listOfFiles = listOfFiles;
+		}
+		
+		/**
+		 * @return the listOfFiles
+		 */
+		public List<FilesForMustach> getListOfFiles() {
+			return listOfFiles;
+		}
+		/**
+		 * @param listOfFiles the listOfFiles to set
+		 */
+		public void setListOfFiles(List<FilesForMustach> listOfFiles) {
+			this.listOfFiles = listOfFiles;
+		}
+//		String username;
+		
+		
+		/*public List<String> getFiles() {
+			return filenames;
+		}*/
+}
