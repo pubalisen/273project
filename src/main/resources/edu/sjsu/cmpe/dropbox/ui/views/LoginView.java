ef05eea6717377bae089f51f51eab91d47ccbcdb
poojaindi <pooja.indi@gmail.com>
2014-05-06 23:35:33 -0700

    Added ui files from /java/ui to /resources/ui

---
+++ src/main/resources/edu/sjsu/cmpe/dropbox/ui/views/LoginView.java
@@ -0,0 +1,12 @@
+package edu.sjsu.cmpe.dropbox.ui.views;
+
+
+import com.yammer.dropwizard.views.View;
+
+public class LoginView extends View {
+
+	public LoginView() {
+		super("Login.mustache");
+	}
+
+}
---
+++ src/main/resources/edu/sjsu/cmpe/dropbox/ui/views/SharedFileView.java
@@ -0,0 +1,30 @@
+package edu.sjsu.cmpe.dropbox.ui.views;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.yammer.dropwizard.views.View;
+
+public class SharedFileView extends View{
+
+	//private List<String> filenames;
+		List<FilesForMustach> listOfFiles =new ArrayList<FilesForMustach>();
+		
+		public SharedFileView(List<FilesForMustach> listOfFiles) {
+			super("SharedFile.mustache");
+			this.listOfFiles = listOfFiles;
+		}
+		
+		/**
+		 * @return the listOfFiles
+		 */
+		public List<FilesForMustach> getListOfFiles() {
+			return listOfFiles;
+		}
+		/**
+		 * @param listOfFiles the listOfFiles to set
+		 */
+		public void setListOfFiles(List<FilesForMustach> listOfFiles) {
+			this.listOfFiles = listOfFiles;
+		}
+}
